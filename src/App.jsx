de8b725ebe1ad4d7556bc3067005db175579b3b5
A-Melnykk <969696245n@gmail.com>
2026-02-28 11:40:46 +0200

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,48 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 
 export const App = () => {
-  const [count] = useState(0);
+  const [count, setCount] = useState(0);
 
   const addOne = () => {
-    // write code here
+    setCount(prevCount => prevCount + 1);
   };
 
   const add100 = () => {
-    // write code here
+    setCount(prevCount => prevCount + 100);
   };
 
-  // DON'T change the code below
-  const increase = () => {
-    if (count % 5 === 0) {
-      add100();
-    }
+  const handleIncrease = () => {
+    const shouldAddHundred = count % 5 === 0;
 
     addOne();
+
+    if (shouldAddHundred) {
+      add100();
+    }
   };
 
   return (
     <div className="App">
-      <h1 className="App__title">{`Count: ${count}`}</h1>
+      <h1 className="App__title">Count: {count}</h1>
 
-      <button type="button" className="App__add-one" onClick={addOne}>
-        Add 1
-      </button>
+      <div className="App__controls">
+        <button type="button" className="App__add-one" onClick={addOne}>
+          Add 1
+        </button>
 
-      <button type="button" className="App__add-100" onClick={add100}>
-        Add 100
-      </button>
+        <button type="button" className="App__add-100" onClick={add100}>
+          Add 100
+        </button>
 
-      <button type="button" className="App__increase" onClick={increase}>
-        Increase
-      </button>
+        <button
+          type="button"
+          className="App__increase"
+          onClick={handleIncrease}
+        >
+          Increase
+        </button>
+      </div>
     </div>
   );
 };
